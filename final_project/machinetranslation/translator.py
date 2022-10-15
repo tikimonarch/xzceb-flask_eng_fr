1280c36d1f2e67c32d2a761ee93b0d19762e80fc
tikimonarch <tikimonarch@gmail.com>
2022-10-15 11:08:52 -0400

    Final changes2

--- final_project/machinetranslation/translator.py
+++ final_project/machinetranslation/translator.py
@@ -10,15 +10,15 @@ load_dotenv()
 apikey = os.environ['apikey']
 url = os.environ['url']
 
+authenticator = IAMAuthenticator(apikey)
+language_translator = LanguageTranslatorV3(
+    version='2018-05-01',
+    authenticator=authenticator
+)
+language_translator.set_service_url(url)
+
 def english_to_french(english_text):
     "Translates from english to french"
-    authenticator = IAMAuthenticator(apikey)
-    language_translator = LanguageTranslatorV3(
-        version='2018-05-01',
-        authenticator=authenticator
-    )
-    language_translator.set_service_url(url)
-
     translation = language_translator.translate(
         text=english_text,
         model_id='en-fr').get_result()
@@ -27,13 +27,6 @@ def english_to_french(english_text):
 
 def french_to_english(french_text):
     "Translates from french to english"
-    authenticator = IAMAuthenticator(apikey)
-    language_translator = LanguageTranslatorV3(
-        version='2018-05-01',
-        authenticator=authenticator
-    )
-    language_translator.set_service_url(url)
-
     translation = language_translator.translate(
         text=french_text,
         model_id='fr-en').get_result()
